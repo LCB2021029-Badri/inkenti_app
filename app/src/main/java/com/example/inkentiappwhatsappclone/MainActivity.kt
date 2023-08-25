4e2299d867cfb2b00443572024d9ada9dfdad5c3
badriakkala <lcb2021029@iiitl.ac.in>
2023-08-26 00:33:44 +0530

    successful viewPager in MainActivity.kt

--- app/src/main/java/com/example/inkentiappwhatsappclone/MainActivity.kt
+++ app/src/main/java/com/example/inkentiappwhatsappclone/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.inkentiappwhatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.inkentiappwhatsappclone.adapter.ViewPagerAdapter
+import com.example.inkentiappwhatsappclone.databinding.ActivityMainBinding
+import com.example.inkentiappwhatsappclone.ui.CallFragment
+import com.example.inkentiappwhatsappclone.ui.ChatFragment
+import com.example.inkentiappwhatsappclone.ui.StatusFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentArrayList = ArrayList<Fragment>()
+        fragmentArrayList.add(ChatFragment())
+        fragmentArrayList.add(StatusFragment())
+        fragmentArrayList.add(CallFragment())
+        val adapter = ViewPagerAdapter(this@MainActivity,supportFragmentManager,fragmentArrayList)
+        binding.viewPager.adapter = adapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+
     }
 }
---
+++ app/src/main/java/com/example/inkentiappwhatsappclone/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.inkentiappwhatsappclone.adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(
+    private val context: Context,
+    fm:FragmentManager?,
+    val list:ArrayList<Fragment>
+) : FragmentPagerAdapter(fm!!) {
+    override fun getCount(): Int {
+
+        //return list.size
+        //list size is 3 in this case
+        return 3;
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return list[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        //return super.getPageTitle(position)
+        return TAB_TITLES[position]
+    }
+
+    companion object{
+        val TAB_TITLES = arrayOf("Chats","Status","Call")
+    }
+
+}
