68c4e66bc9d18a0ca76e66f9c31c669eee2cd5d8
badriakkala <lcb2021029@iiitl.ac.in>
2023-08-26 15:52:06 +0530

    added plugins and required dependencies form firebase and github

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,4 +48,26 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    //auth
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    //realtime db
+    implementation("com.google.firebase:firebase-database-ktx")
+
+    //storage
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //circle imageview
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+
+
+
 }
