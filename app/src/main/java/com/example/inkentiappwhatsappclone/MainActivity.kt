e32849b4c2da766d17edac9edec623fe62dd4772
badriakkala <lcb2021029@iiitl.ac.in>
2023-08-26 17:15:05 +0530

    activity_number.xml and NumberActivity not completed

--- app/src/main/java/com/example/inkentiappwhatsappclone/MainActivity.kt
+++ app/src/main/java/com/example/inkentiappwhatsappclone/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.example.inkentiappwhatsappclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.inkentiappwhatsappclone.activity.NumberActivity
 import com.example.inkentiappwhatsappclone.adapter.ViewPagerAdapter
 import com.example.inkentiappwhatsappclone.databinding.ActivityMainBinding
 import com.example.inkentiappwhatsappclone.ui.CallFragment
 import com.example.inkentiappwhatsappclone.ui.ChatFragment
 import com.example.inkentiappwhatsappclone.ui.StatusFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        //i f no one is logged in, go to login page
+        if(auth.currentUser == null){
+            val intent = Intent(this@MainActivity,NumberActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         //view pager setup
         val fragmentArrayList = ArrayList<Fragment>()
--- app/src/main/java/com/example/inkentiappwhatsappclone/activity/NumberActivity.kt
+++ app/src/main/java/com/example/inkentiappwhatsappclone/activity/NumberActivity.kt
@@ -1,12 +1,59 @@
 package com.example.inkentiappwhatsappclone.activity
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.inkentiappwhatsappclone.MainActivity
 import com.example.inkentiappwhatsappclone.R
+import com.example.inkentiappwhatsappclone.databinding.ActivityNumberBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class NumberActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNumberBinding
+    private lateinit var auth :FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_number)
+        binding = ActivityNumberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser != null){
+            val intent = Intent(this@NumberActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        //else
+        binding.btnSendOTP.setOnClickListener {
+            if(binding.etPhoneNumber.text!!.isEmpty()){
+                    createSnackBar(it)
+            }else{
+                var intent = Intent(this@NumberActivity,OTPActivity::class.java)
+                intent.putExtra("number",binding.etPhoneNumber.text!!)
+                startActivity(intent)
+            }
+        }
+
     }
+
+
+    private fun createSnackBar(view:View){
+        Snackbar.make(view,"Phone number can't be empty",Snackbar.LENGTH_INDEFINITE)
+            .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
+            .setBackgroundTint(Color.parseColor("#006400"))
+            .setAction("Edit"){
+//                Toast.makeText(this,"snackbar button pressed",Toast.LENGTH_SHORT).show()
+            }
+            .show()
+    }
+
+
 }
