06b2106867cfd3f9424014424ce7eaf9d139886c
badriakkala <lcb2021029@iiitl.ac.in>
2023-08-27 19:39:45 +0530

    ProfileActivity.kt - getting user image through intent - successful

--- app/src/main/java/com/example/inkentiappwhatsappclone/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/inkentiappwhatsappclone/activity/ProfileActivity.kt
@@ -1,12 +1,64 @@
 package com.example.inkentiappwhatsappclone.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.inkentiappwhatsappclone.R
+import com.example.inkentiappwhatsappclone.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityProfileBinding
+    private lateinit var auth:FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var storage: FirebaseStorage
+    private lateinit var selectedImg :Uri
+    private lateinit var dialog: AlertDialog.Builder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding  = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initializing all
+        dialog = AlertDialog.Builder(this)
+            .setMessage("Updating Profile ...")
+            .setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        binding.userImage.setOnClickListener {
+            getImageFormGallery()
+        }
+
+
+
     }
+
+
+    private fun getImageFormGallery(){
+        //implicit intent for image
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 69)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(data!=null){
+            if(data.data!=null){
+                selectedImg = data.data!!
+                binding.userImage.setImageURI(selectedImg)
+            }
+        }
+    }
+
+
+
 }
