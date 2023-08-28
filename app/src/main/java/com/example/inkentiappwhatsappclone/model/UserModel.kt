b817b8109048ed7744ae2475b7ad838a05facb84
badriakkala <lcb2021029@iiitl.ac.in>
2023-08-28 15:54:48 +0530

    UserModel.kt - no value constructor is added - (fixed)

--- app/src/main/java/com/example/inkentiappwhatsappclone/model/UserModel.kt
+++ app/src/main/java/com/example/inkentiappwhatsappclone/model/UserModel.kt
@@ -1,8 +1,8 @@
 package com.example.inkentiappwhatsappclone.model
 
 data class UserModel(
-    val uid : String,
-    val name:String,
-    val number:String,
-    val imageUrl:String
+    val uid : String? = "",
+    val name:String? = "",
+    val number:String? = "",
+    val imageUrl:String? = ""
 )
